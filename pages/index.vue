3c937439ed46bdadc78f21a1ee7c234bbf5779a8
Charlotte Fereday <cbrfereday@gmail.com>
2021-09-12 18:40:51 +0100

    feat: adds skeleton api call

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <Tutorial/>
+  <div>
+  <p> Here is a quote</p>
+  {{ this.myQuote }}
+  </div>
 </template>
 
-<script>
-export default {}
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return { myQuote: 'placeholder'}
+  },
+
+   created() {
+     this.gettingQuotes()
+   },
+    methods: {
+    gettingQuotes: async function() {
+    try {
+      let response = await fetch("https://type.fit/api/quotes")
+      if (!response.ok) {
+        throw new Error('Could not fetch quotes')
+      }
+
+      let text =  await response.text();
+      this.myQuote = text;
+
+    } catch(e) {
+      console.log('Here is the error', e)
+    }
+  }
+}
+});
+
 </script>
